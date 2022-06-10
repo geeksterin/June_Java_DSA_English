1c278531c9a2f2e9de7d86e1c78f3b74ba9aa880
vidyashree-muralidhara <69386960+vidyashree-muralidhara@users.noreply.github.com>
2022-06-10 20:21:07 +0530

    test checkin

---
+++ June_DSA_Practise_Programs_Git/src/test/pkg/HelloWorldTest.java
@@ -0,0 +1,11 @@
+package test.pkg;
+
+public class HelloWorldTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World!");
+	}
+
+}
