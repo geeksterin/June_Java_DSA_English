28f298b4785e739713f367b7f3bf89dfd7d8321e
vidyashree-muralidhara <69386960+vidyashree-muralidhara@users.noreply.github.com>
2022-07-08 22:45:09 +0530

    TimeComplexity examples

---
+++ June_DSA_Practise_Programs_Git/dsa_8th_July_2022/BubbleSortDecreasingOrder.java
@@ -0,0 +1,32 @@
+package dsa_8th_July_2022;
+
+import java.util.Arrays;
+
+public class BubbleSortDecreasingOrder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[] = { 4, 3, 2, 1 };
+
+		for (int i = 0; i < a.length - 1; i++) {
+			System.out.println("------------------");
+			System.out.println("Pass -> " + (i + 1));
+			for (int j = 0; j < a.length - 1 - i; j++) {
+				if (a[j] < a[j + 1]) {
+					// swap
+					int temp = a[j];
+					a[j] = a[j + 1];
+					a[j + 1] = temp;
+				}
+
+				System.out.println(Arrays.toString(a));
+
+			}
+		}
+
+		System.out.println();
+		System.out.println("----END RESULT ----");
+		System.out.println(Arrays.toString(a));
+	}
+
+}
---
+++ June_DSA_Practise_Programs_Git/dsa_8th_July_2022/FindSumofNNumbers.java
@@ -0,0 +1,15 @@
+package dsa_8th_July_2022;
+
+public class FindSumofNNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		long n = 214567819;
+		
+		long finalSum = n*(n+1)/2;
+		
+		System.out.println(finalSum);
+	}
+
+}
