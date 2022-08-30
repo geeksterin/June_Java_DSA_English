d43721ecb336be418c3e5eabcd6f93c85370e8d3
vidyashree-muralidhara <69386960+vidyashree-muralidhara@users.noreply.github.com>
2022-08-30 22:47:07 +0530

    Class and Object creation examples

---
+++ June_DSA_Practise_Programs_Git/dsa_30th_Aug_2022/ClientStudent.java
@@ -0,0 +1,35 @@
+package dsa_30th_Aug_2022;
+
+public class ClientStudent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Student s1 = new Student();
+		s1.name = "Aman";
+		s1.id = 100;
+		
+		Student s2 = new Student();
+		s2.name = "Akash";
+		s2.id = 200;
+		
+		Student s3 = new Student();
+		s3.name = "Karan";
+		s3.id = 300;
+		
+		Student s4 = new Student();
+		s4.name = "Karan";
+		s4.id = 300;
+		
+		Student s5 = new Student();
+		s5.name = "Karan";
+		s5.id = 300;
+		
+	
+
+
+
+		
+	}
+
+}
---
+++ June_DSA_Practise_Programs_Git/dsa_30th_Aug_2022/Student.java
@@ -0,0 +1,22 @@
+package dsa_30th_Aug_2022;
+
+public class Student {
+	String name;
+	int id;
+	String batch;
+
+
+	Student(){
+		id = 100;
+	}
+	
+	
+	Student(String name1, int id1){
+		name = name1;
+		id = id1;
+	}
+	
+	
+}
+
+
