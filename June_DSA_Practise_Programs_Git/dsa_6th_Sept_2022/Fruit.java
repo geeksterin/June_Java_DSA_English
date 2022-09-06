1e58bcd26f46bfa7fdc315902dc7e4c075fccc75
vidyashree-muralidhara <69386960+vidyashree-muralidhara@users.noreply.github.com>
2022-09-06 23:07:08 +0530

    variables in java

---
+++ June_DSA_Practise_Programs_Git/dsa_6th_Sept_2022/Fruit.java
@@ -0,0 +1,19 @@
+class Fruit{
+
+String colour;
+String taste;
+
+void m1(String t){
+{
+
+String temp = "";
+for(int i=0;i<4;i++){
+temp = temp + s.charAt(i);
+}
+}
+
+void m2(){
+
+temp = temp+"hello" ; // not allowed as temp's scope is only in m1 method
+}
+}
